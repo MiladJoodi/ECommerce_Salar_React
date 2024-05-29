0aecf77b52f603da62630fd304716a2b57e723a6
Milad Joodi <winbeta.com@gmail.com>
2024-05-29 13:32:40 +0330

    Update Navbar.tsx

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,20 +1,28 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
+import Container from "../container/Container";
 
 const Navbar = () => {
   return (
-    <div className='h-14 border-b shadow flex justify-between flex-row-reverse items-center'>
-        <ul className='flex'>
-            <li><Link to="/">خانه</Link></li>
-            <li><Link to="/store">فروشگاه</Link></li>
-        </ul>
+    <div className="h-14 border-b shadow flex items-center">
+      <Container>
+        <div className="flex justify-between flex-row-reverse items-center">
+          <ul className="flex">
+            <li>
+              <Link to="/">خانه</Link>
+            </li>
+            <li>
+              <Link to="/store">فروشگاه</Link>
+            </li>
+          </ul>
 
-        <div>
+          <div>
             <button>سبد خرید</button>
+          </div>
         </div>
-
+      </Container>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
