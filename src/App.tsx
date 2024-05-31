64f885291e3491377fad5af65739b4535ca39eda
Milad Joodi <winbeta.com@gmail.com>
2024-05-31 23:22:34 +0330

    up

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Store from "./pages/store/Store";
 import Layout from "./components/layout/Layout";
 import Product from "./pages/product/Product";
 import Cart from "./pages/cart/Cart";
-
+// npx json-server data/db.json -p 8001
 function App() {
   return (
     <>
