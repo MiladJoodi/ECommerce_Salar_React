267ff6421c9cc5bee583b68e959e1b92156781b2
Milad Joodi <winbeta.com@gmail.com>
2024-06-05 16:26:39 +0330

    up

--- src/services/api.ts
+++ src/services/api.ts
@@ -4,8 +4,16 @@ const client = axios.create({
     baseURL: "http://localhost:8001"
 })
 
+// Get All Products
 export async function getProducts(){
     const {data} = await client("/products");
+
+    return data;
+}
+
+// Get Single Product
+export async function getProduct(id: string | number){
+    const {data} = await client(`/products/${id}`);
 
     return data;
 }
