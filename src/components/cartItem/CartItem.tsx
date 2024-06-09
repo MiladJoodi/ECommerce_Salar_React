274af2df44908c7dd7ad99d159bf83ded36bf1d5
Milad Joodi <winbeta.com@gmail.com>
2024-06-09 21:19:43 +0330

    Update CartItem.tsx

--- src/components/cartItem/CartItem.tsx
+++ src/components/cartItem/CartItem.tsx
@@ -3,6 +3,7 @@ import Button from "../button/Button";
 import { getProduct } from "../../services/api";
 import { IProduct } from "../../types/servers";
 import { useShoppingCartContext } from "../../context/ShoppingCartContext";
+import { Link } from "react-router-dom";
 
 interface ICartItem {
   id: number;
@@ -26,16 +27,18 @@ function CartItem({ id, qty }: ICartItem) {
 
   return (
     <div className="flex flex-row-reverse mt-4 border-b pb-2">
-      <img className="rounded w-28" src={product?.image} alt="" />
+      <Link to={`/product/${id}`}>
+        <img className="rounded w-28" src={product?.image} alt="" />
+      </Link>
 
       <div className="mr-4">
         <h3 className="text-right">{product?.title}</h3>
 
         <div className="mt-2">
-          <Button 
-          className="mr-2" 
-          variant="danger"
-          onClick={()=> handleRemoveProduct(id)}
+          <Button
+            className="mr-2"
+            variant="danger"
+            onClick={() => handleRemoveProduct(id)}
           >
             Remove
           </Button>
