12a234c94ec98ace53d5717dc324b99ae219ed2d
Milad Joodi <winbeta.com@gmail.com>
2024-06-09 21:15:40 +0330

    Update CartItem.tsx

--- src/components/cartItem/CartItem.tsx
+++ src/components/cartItem/CartItem.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Button from "../button/Button";
 import { getProduct } from "../../services/api";
 import { IProduct } from "../../types/servers";
+import { useShoppingCartContext } from "../../context/ShoppingCartContext";
 
 interface ICartItem {
   id: number;
@@ -9,33 +10,50 @@ interface ICartItem {
 }
 
 function CartItem({ id, qty }: ICartItem) {
+  const [product, setProduct] = useState<IProduct>();
 
-  const [product, setProduct] = useState<IProduct>()
+  const {
+    handleIncreaseProductQty,
+    handleDecreaseProductQty,
+    handleRemoveProduct,
+  } = useShoppingCartContext();
 
-  useEffect(()=>{
-    getProduct(id).then((data)=> {
-      setProduct(data)
-    })
-  }, [])
+  useEffect(() => {
+    getProduct(id).then((data) => {
+      setProduct(data);
+    });
+  }, []);
 
   return (
     <div className="flex flex-row-reverse mt-4 border-b pb-2">
-      <img
-        className="rounded w-28"
-        src="https://images.pexels.com/photos/17180127/pexels-photo-17180127/free-photo-of-clouds-over-mountains-in-winter.jpeg"
-        alt=""
-      />
+      <img className="rounded w-28" src={product?.image} alt="" />
 
       <div className="mr-4">
-        <h3 className="text-right">عنوان محصول</h3>
+        <h3 className="text-right">{product?.title}</h3>
 
         <div className="mt-2">
-          <Button className="mr-2" variant="danger">
+          <Button 
+          className="mr-2" 
+          variant="danger"
+          onClick={()=> handleRemoveProduct(id)}
+          >
             Remove
           </Button>
-          <Button variant="primary">+</Button>
+          <Button
+            variant="primary"
+            onClick={() => handleIncreaseProductQty(id)}
+          >
+            +
+          </Button>
+
           <span className="mx-2">{qty}</span>
-          <Button variant="primary">-</Button>
+
+          <Button
+            variant="primary"
+            onClick={() => handleDecreaseProductQty(id)}
+          >
+            -
+          </Button>
         </div>
       </div>
     </div>
